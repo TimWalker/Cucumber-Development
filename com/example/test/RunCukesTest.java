3dbb9b9f9f0614b63105700e21f8ad4e07a13f3c
Tim Walker <walketim@gmail.com>
2015-11-22 12:20:14 -0600

    added a few features and steps

--- com/example/test/RunCukesTest.java
+++ com/example/test/RunCukesTest.java
@@ -2,9 +2,11 @@ package com.example.test;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features="features")
 public class RunCukesTest {
 }
 
